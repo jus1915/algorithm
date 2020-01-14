6b1bae062c5149b21262873e3a05fa685b895b00
jus1915 <59350228+jus1915@users.noreply.github.com>
2020-01-14 13:23:28 +0900

    2578 빙고

--- Main.java
+++ Main.java
@@ -1,18 +1,50 @@
-package sec01.exam01;
-import java.util.Scanner;
+package project1;
+import java.util.*;
 public class Main {
-	public int RecFunc(int n) {
-		if(n==0)
-			return 0;
-		else if(n==1)
-			return 1;
-		return RecFunc(n-1)+RecFunc(n-2);
-		
-	}
 	public static void main(String[] args) {
-		Main m=new Main();
+		Main app=new Main();
 		Scanner sc=new Scanner(System.in);
-		int n=sc.nextInt();
-		System.out.println(m.RecFunc(n));
+		int[][] a=new int[5][5];
+		for(int i=0;i<5;i++) {
+			for(int j=0;j<5;j++) {
+				a[i][j]=sc.nextInt();
+			}
+		}
+		for(int i=1;i<25;i++) {
+			int r=0,c=0,cr=0,rcr=0,cnt=0;
+			int n=sc.nextInt();
+			for(int x=0;x<5;x++) {
+				for(int y=0;y<5;y++) {
+					if(a[x][y]==n)
+						a[x][y]=0;
+				}
+			}
+			for(int j=0;j<5;j++) {
+				r=0;
+				for(int k=0;k<5;k++) {
+					if(a[j][k]==0) r++;
+					if(r==5) cnt++;
+				}
+			}
+			for(int j=0;j<5;j++) {
+				c=0;
+				for(int k=0;k<5;k++) {
+					if(a[k][j]==0) c++;
+					if(c==5) cnt++;
+				}
+			}
+			for(int j=0;j<5;j++) {
+				if(a[j][j]==0) cr++;
+				if(cr==5) cnt++;
+			}
+			for(int j=0;j<5;j++) {
+				if(a[j][4-j]==0) rcr++;
+				if(rcr==5) cnt++;
+			}
+			if(cnt>=3) {
+				System.out.println(i);
+				break;
+			}
+		}
 	}
 }
