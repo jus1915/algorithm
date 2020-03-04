4147b1bc6f73d8afebbffff916f5664228137379
jus1915 <59350228+jus1915@users.noreply.github.com>
2020-03-04 21:44:50 +0900

    2751 수 정렬하기 2

---
+++ 2751.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int n, a;
+    vector <int> v;
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a;
+        v.push_back(a);
+    }
+
+    sort(v.begin(), v.end());
+
+    for (int i = 0; i < n; i++)
+        cout << v[i] << "\n";
+
+    return 0;
+}
