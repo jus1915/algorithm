16bfae6dbce83b23bf68d24d3b8615bec4cb61c8
jus1915 <59350228+jus1915@users.noreply.github.com>
2020-03-04 21:50:03 +0900

    피보나치 함수

---
+++ 1003.cpp
@@ -0,0 +1,26 @@
+﻿#include<iostream>
+using namespace std;
+int zero[40];
+int one[40];
+void fibonacci(int p);
+int main(void) {
+    int n;
+    cin >> n;
+    int k;
+    for (int i = 0; i < n; i++) {
+        cin >> k;
+        fibonacci(k);
+    }
+    return 0;
+}
+void fibonacci(int p) {
+    zero[0] = 1;
+    zero[1] = 0;
+    one[0] = 0;
+    one[1] = 1;
+    for (int i = 2; i <= p; i++) {
+        zero[i] = zero[i - 1] + zero[i - 2];
+        one[i] = one[i - 1] + one[i - 2];
+    }
+    cout << zero[p] << " " << one[p] << endl;
+}
