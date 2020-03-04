8d6cde6fd2aa3095458feb4a0d313a4f584e747b
jus1915 <59350228+jus1915@users.noreply.github.com>
2020-03-04 21:54:27 +0900

    달팽이는 올라가고 싶다

--- 2869.cpp
+++ 2869.cpp
@@ -1,12 +1,14 @@
 #include<iostream>
+#include<stdio.h>
 using namespace std;
 int main(void) {
 	int a, b, v, day;
+	int day=0;
 	cin >> a >> b >> v;
 	if ((v - a) % (a - b) == 0)
-		day = (v - a) / (a - b) + 1;
+		day = (v - a) / (a - b)+1;
 	else
-		day = (v - a) / (a - b) + 2;
-	cout << day << endl;
+		day= (v - a) / (a - b) + 2;
+	printf("%d", day);
 	return 0;
 }
